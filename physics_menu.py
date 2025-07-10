1b663e1fce7be8b2e59e9c67fe0a2212adad1708
UnityIsHardToLearn <101976283+UnityIsHardToLearn@users.noreply.github.com>
2025-07-10 11:34:27 +0900

    Update physics_menu.py

--- physics_menu.py
+++ physics_menu.py
@@ -1,19 +1,19 @@
-phy_menu = [
-    "Wave equation",
+physics_menu = [
+    "Wave Equation",
     "Half life",
-    "Final velocity"]
+    "Final Velocity"]
 
-for i in range(len(phy_menu)):
-    print(str(i+1) + "." + " " + phy_menu[i])
+for i in range(len(physics_menu)):
+    print(str(i+1) + "." + " " + physics_menu[i])
 
 choice = input("Please enter a number: ")
-
-try:
-    choice = int(choice)
-    if 1 <= choice <= len(phy_menu):
-        print(f"You selected: {phy_menu[choice - 1]}")
-    else:
-        print("Invalid choice. Please select a number from the list.")
-except ValueError:
-    print("Invalid input. Please enter a number.")
+while choice == None:
+    try:
+        choice = int(choice)
+        if 1 <= choice <= len(physics_menu):
+            print(f"You selected: {physics_menu[choice - 1]}")
+        else:
+            print("Invalid choice. Please select a number from the list.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
     
