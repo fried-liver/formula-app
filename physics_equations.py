47db0ce833579735613370bf7d52314585a6b323
Haji414 <hajimeshin414@gmail.com>
2025-07-11 09:58:56 +0900

    final velo added

--- physics_equations.py
+++ physics_equations.py
@@ -1,2 +1,19 @@
-def physics_equations():
+import math
+class final_velo:
+    def __init__(self, V=None, u=None, a=None, s=None):
+        self.V = V
+        self.u = u
+        self.a = a
+        self.s = s
     
+    def solve_V(self):
+        V = math.sqrt(self.u**2+2*(self.a*self.s))
+        return round(V, 1)
+    
+    def solve_u(self):
+        u = math.sqrt(self.V**2 - 2*(self.a*self.s))
+        return round(u, 1)
+
+    def solve_a(self):
+        a = (self.V**2 - self.u**2)/(2*self.s)
+        return round(a, 1)
