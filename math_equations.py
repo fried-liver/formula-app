fc85a6aef65a956d960fa2f62c3b32ed1793d4c2
Haji414 <hajimeshin414@gmail.com>
2025-07-11 09:57:18 +0900

    Update math_equations.py

--- math_equations.py
+++ math_equations.py
@@ -60,3 +60,22 @@ class sine:
 
 solve = sine(A=26.6, B=43, a=None, b=3.5)
 print(solve.solve_a())
+
+class final_velo:
+    def __init__(self, V=None, u=None, a=None, s=None):
+        self.V = V
+        self.u = u
+        self.a = a
+        self.s = s
+    
+    def solve_V(self):
+        V = math.sqrt(self.u**2+2*(self.a*self.s))
+        return round(V, 1)
+    
+    def solve_u(self):
+        u = math.sqrt(self.V**2 - 2*(self.a*self.s))
+        return round(u, 1)
+
+    def solve_a(self):
+        a = (self.V**2 - self.u**2)/(2*self.s)
+        return round(a, 1)
