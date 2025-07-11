0746070a6aabb54784f43cf93eb044fc59bf5d5d
jekaykajey <heyitsmuadz@gmail.com>
2025-07-11 11:51:55 +0900

    test

--- physics_menu.py
+++ physics_menu.py
@@ -1,25 +1,52 @@
-from physics_equations import *
-def physics_menu():
+"""def physics_menu():
     physics_menu = [
-        "Wave Equation",
-        "Half life",
-        "Final Velocity"]
+        "Wave equation",
+        "Half-life equation",
+        "Final velocity equation"]
 
     for i in range(len(physics_menu)):
         print(str(i+1) + "." + " " + physics_menu[i])
 
-    choice = input("Please enter a number: ")
-    while choice == None:
+    while True:
+        
         try:
-            choice = int(choice)
+            choice = int(input("Please enter a number: "))
             if 1 <= choice <= len(physics_menu):
+                #physics_equations.math_equation(physics_menu[choice - 1])
                 print(f"You selected: {physics_menu[choice - 1]}")
+                break
             else:
-                print("Invalid choice. Please select a number from the list.")
+                print("Invalid choice, please try again.")
         except ValueError:
-            print("Invalid input. Please enter a number.")
+            print("Invalid input, please enter a number.")"""
+
+import validation
+import physics_equations
+
+def physics_menu():
+    physics_menu = {
+        1 : {
+            "function" : physics_equations.wave_equation(),
+            "variables" : ["speed", "wavelength", "frequency"],
+        },
+        2 : {
+            "function" : physics_equations.final_velo(),
+            "variables" : [""],
+        },
+    }
+
+    for key in physics_menu.keys():
+        print(key)
     
+    while True:
+        try:
+            choice = int(input("Enter your choice: "))
+            if 1 >= choice and choice <= len(physics_menu):
+                for i in physics_menu[choice]["variables"]:
+                    print(i)
 
 
-if __name__=="__main__":
-    pass
+        except ValueError:
+            print("Invalid input")
+
+physics_menu()
